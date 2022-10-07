8ee2dc9e8d77d9e562f0908f015363514789f698
PC-TICS12 <a3519110011@alumno.uttehuacan.edu.mx>
2022-10-07 16:40:19 -0500

    actualizacion

--- SW.js
+++ SW.js
@@ -1,12 +1,12 @@
 var cacheName='appV1';
-var contenidoCache=['index.html', 'app.js', 'sw.js','manifest.webmanifest','bootstrap/css/bootstrap.css', 'bootstrap/js/bootstrap.min.js']
+var contenidoCache=['index.html', 'app.js', 'sw.js','manifest.webmanifest','bootstrap/css/bootstrap.css', 'bootstrap/js/bootstrap.min.js'];
 
 self.addEventListener('install',(e)=>{
 console.log("Instalado")
-e.waitUntil(async()=>{
+e.waitUntil((async()=>{
     const cache=await caches.open(cacheName);
     await cache.addAll(contenidoCache);
-})
+})())
 });
 
 
@@ -18,5 +18,5 @@ self.addEventListener('fetch',(e)=>{
         const cache=await caches.open(cacheName);
         cache.put(e.request, response.clone());
         return response;
-    }))
+    })())
 });
